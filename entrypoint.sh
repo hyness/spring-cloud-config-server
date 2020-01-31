db8cdf441e35d3edb32a5e487763449eb9d021b0
Ben Nelson <ben.nelson@coxautoinc.com>
2020-01-31 16:04:18 -0500

    removing quotes from JAVA_OPTS to make the script run properly

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java "${JAVA_OPTS}" -Djava.security.egd=file:/dev/./urandom -jar \
+java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar \
 /opt/spring-cloud-config-server/target/spring-cloud-config-server.jar --server.port=8888 \
 --spring.config.name=application "$@"
