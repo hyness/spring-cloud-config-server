b9d271419018fe90372c3325654b93dae8d9a366
hyness <hyness@freshlegacycode.org>
2020-08-09 18:46:34 -0700

    Create verification workflow #26

--- examples/redis/populate-redis.sh
+++ examples/redis/populate-redis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo 'HMSET sample-app server.port "8100" sample.topic.name "test" test.property1 "property1"' | redis-cli
+echo 'HMSET redis-app-development server.port "8100" redis.topic.name "test" test.property1 "property1"' | redis-cli
