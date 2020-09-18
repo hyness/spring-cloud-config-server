2368c24b621219b11b568cdab3412e7bd0d0b822
cmayfield <cameron.j.mayfield@disney.com>
2020-09-18 10:56:05 -0700

    Add Spring Cloud Bus support

--- src/main/kotlin/org/freshlegacycode/cloud/config/server/ConfigServerApplication.kt
+++ src/main/kotlin/org/freshlegacycode/cloud/config/server/ConfigServerApplication.kt
@@ -7,10 +7,19 @@ import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.cloud.bus.BusAutoConfiguration
+import org.springframework.cloud.bus.BusPropertiesAutoConfiguration
+import org.springframework.cloud.bus.BusRefreshAutoConfiguration
+import org.springframework.cloud.bus.ServiceMatcherAutoConfiguration
+import org.springframework.cloud.bus.jackson.BusJacksonAutoConfiguration
+import org.springframework.cloud.config.monitor.EnvironmentMonitorAutoConfiguration
 import org.springframework.cloud.config.server.EnableConfigServer
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Profile
+import org.springframework.context.annotation.PropertySource
+import org.springframework.cloud.stream.binder.kafka.config.ExtendedBindingHandlerMappingsProviderConfiguration as KafkaBinderAutoConfiguration
+import org.springframework.cloud.stream.binder.rabbit.config.ExtendedBindingHandlerMappingsProviderConfiguration as RabbitBinderAutoConfiguration
 
 @EnableConfigServer
 @SpringBootApplication(exclude = [
@@ -18,7 +27,15 @@ import org.springframework.context.annotation.Profile
     RedisAutoConfiguration::class,
     ManagementContextAutoConfiguration::class,
     ManagementWebSecurityAutoConfiguration::class,
-    SecurityAutoConfiguration::class
+    SecurityAutoConfiguration::class,
+    RabbitBinderAutoConfiguration::class,
+    KafkaBinderAutoConfiguration::class,
+    BusAutoConfiguration::class,
+    BusPropertiesAutoConfiguration::class,
+    ServiceMatcherAutoConfiguration::class,
+    BusRefreshAutoConfiguration::class,
+    BusJacksonAutoConfiguration::class,
+    EnvironmentMonitorAutoConfiguration::class,
 ])
 class ConfigServerApplication
 
@@ -31,6 +48,24 @@ internal class ActuatorBackendConfiguration
 @Configuration
 @Import(DataSourceAutoConfiguration::class)
 internal class JdbcBackendConfiguration
+
+@Profile("cloud-bus-kafka")
+@Configuration
+@Import(KafkaBinderAutoConfiguration::class)
+internal class CloudBusKafkaConfiguration : CloudBusConfiguration()
+
+@Profile("cloud-bus-rabbit")
+@Configuration
+@Import(RabbitBinderAutoConfiguration::class)
+internal class CloudBusRabbitConfiguration : CloudBusConfiguration()
+
+@Import(BusAutoConfiguration::class,
+        BusPropertiesAutoConfiguration::class,
+        ServiceMatcherAutoConfiguration::class,
+        BusRefreshAutoConfiguration::class,
+        BusJacksonAutoConfiguration::class,
+        EnvironmentMonitorAutoConfiguration::class)
+internal abstract class CloudBusConfiguration
 
 @Profile("redis")
 @Configuration
