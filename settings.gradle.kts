2a696c51c91e04b2f2723038b7e53963b71070d1
hyness <hyness@freshlegacycode.org>
2020-11-12 21:18:22 -0800

    Update to 2.2.6.RELEASE
    - More build.gradle.kts cleanup
    - Update to gradle 6.7
    - Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,11 @@ rootProject.name = "spring-cloud-config-server"
 pluginManagement {
     val kotlinVersion: String by settings
     val springBootVersion: String by settings
+    val springDependencyManagement: String by settings
 
     plugins {
         id("org.springframework.boot") version springBootVersion
-        id("io.spring.dependency-management") version "1.0.10.RELEASE"
+        id("io.spring.dependency-management") version springDependencyManagement
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.spring") version kotlinVersion
     }
