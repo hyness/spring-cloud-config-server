a1d9fc10606db11b590c616fcac95757ec33ad8e
hyness <hyness@freshlegacycode.org>
2021-05-30 19:51:38 -0700

    Update to 3.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-bus")
     implementation("org.springframework.cloud:spring-cloud-starter-bus-kafka")
     implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework:spring-jdbc")
